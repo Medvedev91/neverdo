12fe4f6e5dc2cae254b17d9e5035f1cd843a874a
hi@timeto.me <hi@timeto.me>
2024-01-01 13:50:26 +0300

    build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+buildscript {
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+    }
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
